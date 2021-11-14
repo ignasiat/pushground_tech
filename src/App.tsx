912555f1636e5a3f536ed3ea817dabb957ca4c03
ignasi <ignasi.amargos@gmail.com>
2021-11-14 07:43:15 +0100

    Initial redirect to login working

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  BrowserRouter, Route, Routes,
+  BrowserRouter, Route, Routes, Navigate
 } from 'react-router-dom'
 import './App.scss';
 import Login from './Login';
@@ -12,13 +12,15 @@ import Dashboard from './Dashboard'
 
 function App(): JSX.Element {
   return (
+    
     <BrowserRouter>
     <Header />
       <Routes>
+        <Route path="*" element={<Navigate replace to="/login" />} />
         <Route path="/dashboard" element={<Dashboard />} />  
         <Route path="/features" element={<Features />} />
         <Route path="/blog"  element={<Blog/>} />
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
