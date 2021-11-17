640e5386f27ef18eccf4c7f9b881fa31b0916408
ignasi <ignasi.amargos@gmail.com>
2021-11-17 07:23:09 +0100

    Add logout button to header

--- src/Header/index.tsx
+++ src/Header/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../AuthSystem';
+import './styles.scss';
 
 const Header = ():JSX.Element => {
+  const navigate = useNavigate();
+  const { isLogged, signout } = useAuth();
   return (
-  <ul>
-    <li><Link to='/blog'>Blog</Link></li>
-    <li><Link to='/features'>Features</Link></li>
-    <li><Link to='/dashboard'>Dashboard</Link></li>
-  </ul>)
+    <div className='header'>
+      <nav className='navbar'>
+        <img src='https://app.pushground.com/assets/pushground.png' alt='logo pushground' />
+        <ul className='list'>
+          <li><Link to='/blog'>Blog</Link></li>
+          <li><Link to='/features'>Features</Link></li>
+          <li><Link to='/dashboard'>Dashboard</Link></li>
+        </ul>
+      </nav>
+    {isLogged && <button className='button--logout' onClick={() => signout(() => navigate('/dashboard'))}>Logout</button>}
+  </div>
+  )
 }
 
 export default Header;
